6502c601dc103baa362378c74860b7285c7e1fbe
miketwheeler <mikeytwheeler@gmail.com>
2021-04-24 22:16:32 -0700

    added style, navigation, meetup page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import mainNavStyle from './MainNavigation.module.css'
 
 function MainNavigation() {
 	return (
-		<header>
-			<div>React Meetups</div>
+		<header className={mainNavStyle.header}>
+			<div className={mainNavStyle.logo}>React Meetups</div>
 			<nav>
 				<ul>
 					<li>
