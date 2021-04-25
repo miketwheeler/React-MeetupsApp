9d53a8ad3842589b50c718832c411a095705f981
miketwheeler <mikeytwheeler@gmail.com>
2021-04-24 19:53:12 -0700

    inital pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import Favorites from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  return( 
+	<div>
+		<MainNavigation />
+		<Switch>
+			<Route path='/' exact>
+				<AllMeetupsPage />
+			</Route>
+			<Route path='/new-meetup'>
+				<NewMeetupPage />
+			</Route>
+			<Route path='/favorites'>
+				<Favorites />
+			</Route>
+		</Switch>
+	</div>
   );
 }
 
---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllMeetupsPage() {
+	return (
+		<div>
+			All Meetups Page
+		</div>
+	)
+}
+
+export default AllMeetupsPage;
